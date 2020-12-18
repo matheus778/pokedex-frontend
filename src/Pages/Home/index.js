7634ba6a5223c2bc8eda024b71216fee7141e40c
matheus778 <matheuzv777@gmail.com>
2020-12-18 00:36:39 -0300

    estilizando tela de home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './style.css'
 
 export default function Home() {
   return(
-    <h1>home</h1>
+    <div className="container">
+      <nav className="navbar">
+        <h1>Pokedex</h1>
+      </nav>
+
+      <main>
+          <div className="card">
+
+            <div className="card-title">
+              <h1>pokemon</h1>
+            </div>
+
+            <div className="card-image">
+              <img src="https://images-americanas.b2w.io/produtos/01/00/img/1511899/3/1511899345_1SZ.jpg"></img>
+            </div>
+
+            <div>
+              <h3>tipo</h3>
+              <h3>descrição</h3>
+            </div>
+          </div>
+          
+      </main>
+    </div>
   )
 }
