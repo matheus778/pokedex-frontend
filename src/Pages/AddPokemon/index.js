5af3794fd9cc7ef694b4788c8fe8f2e09dcf0651
matheus778 <matheuzv777@gmail.com>
2020-12-18 20:34:36 -0300

    adicionado a funçao de cadastro

--- src/Pages/AddPokemon/index.js
+++ src/Pages/AddPokemon/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import api from '../../Services/api';
 import { FiArrowLeft } from 'react-icons/fi';
 import {Link} from 'react-router-dom';
 import './style.css';
 
 export default function AddPokemon() {
+  
+  function cadastrarPokemon() {
+    let nome = document.querySelector("#nome").value;
+    let tipo = document.querySelector("#tipo").value;
+    let descricao = document.querySelector("#descricao").value;
+    let imagem = document.querySelector("#imagem").value;
+
+    const data = {
+      nome: nome,
+      tipo: tipo,
+      descricao: descricao,
+      imagem: imagem
+    };
+    api.post('/pokemons', data).then(res =>{
+      alert('cadastro efetuado com sucesso')
+    })
+  }
+  
   return(
     <div className="container-form"> 
        <nav className="navbar">
@@ -15,13 +33,13 @@ export default function AddPokemon() {
       </nav>
       <h1>Adicionar novo pokemon</h1>
       <main className="form">
-        <form>
-          <input placeholder="nome"/>
-          <input placeholder="tipo"/>
-          <input placeholder="descricão"/>
-          <input placeholder="imagem url"/>
-          <button>Cadastrar</button>
-        </form>
+        <div className="form-content">
+          <input id="nome" placeholder="nome"/>
+          <input id="tipo" placeholder="tipo"/>
+          <input id="descricao" placeholder="descricão"/>
+          <input id="imagem" placeholder="imagem url"/>
+          <button onClick={()=>(cadastrarPokemon())}>Cadastrar</button>
+        </div>
       </main>
     </div>
   );
